a5f1abbe0e59e7fc07b9873e970ff543a75e2c7e
MAM Distribuidora <mamfilhos29@gmail.com>
2026-06-23 15:54:28 +0100

    Refactor Firebase initialization and update storage bucket
    Updated Firebase initialization and storage bucket URL.

--- firebase.js
+++ firebase.js
@@ -1,19 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyALJUV1EAmwFvDdVmG3wK8KUATcOS8_-dI",
   authDomain: "mam-distribuidora-6043f.firebaseapp.com",
   projectId: "mam-distribuidora-6043f",
-  storageBucket: "mam-distribuidora-6043f.firebasestorage.app",
+  storageBucket: "mam-distribuidora-6043f.appspot.com",
   messagingSenderId: "467923017917",
   appId: "1:467923017917:web:7361ecf8cff2cbfed197d4"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
